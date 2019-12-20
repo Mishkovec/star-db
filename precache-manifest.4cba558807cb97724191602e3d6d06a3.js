6ffcf511ea93a776436635851fc276e9d3be9540
Mishkovec <neilsa@mail.ru>
2019-12-20 16:37:54 +0300

    Updates

---
+++ precache-manifest.4cba558807cb97724191602e3d6d06a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c3b8185699ff7cf30918a98aa00cd2",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "172dff39bdf152591f75",
+    "url": "/star-db/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "bcd8392986ba9988efaa",
+    "url": "/star-db/static/css/main.4d700525.chunk.css"
+  },
+  {
+    "revision": "172dff39bdf152591f75",
+    "url": "/star-db/static/js/2.2007d4b3.chunk.js"
+  },
+  {
+    "revision": "bcd8392986ba9988efaa",
+    "url": "/star-db/static/js/main.53b99a68.chunk.js"
+  },
+  {
+    "revision": "6652a890a4a7b9ef258e",
+    "url": "/star-db/static/js/runtime-main.aff9c821.js"
+  }
+]);
